afd5126393e0ac609da62a172e7f75cb173cbd54
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2025-03-18 10:11:17 +0100

    scaff. options added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\MovieController as AdminMovieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware("auth")->prefix("/admin")->name("admin.")->group(function(){
+    Route::get('/movies', [AdminMovieController::class, 'index'])->name('movies.index');
+    Route::post('/movies', [AdminMovieController::class, 'store'])->name('movies.store');
+    Route::get('/movies/create', [AdminMovieController::class, 'create'])->name('movies.create');
+    Route::get('/movies/{id}', [AdminMovieController::class, 'show'])->name('movies.show');
+    Route::get("/movies/{id}/edit", [AdminMovieController::class, "edit"])->name("movies.edit");
+    Route::put("/movies/{id}", [AdminMovieController::class, "update"])->name("movies.update");
+    Route::delete("/movies/{id}", [AdminMovieController::class, "destroy"])->name("movies.delete");
+});
